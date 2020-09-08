e4dbeb0779af15ec89b896dc92c7d486973c8b38
Juan Vicente <elchente23@gmail.com>
2020-09-07 22:49:02 -0600

    Se agrega la carpeta docs

---
+++ docs/precache-manifest.445cd43873402f898ce04703c375af32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05cb1cadc2a52880e3b758fdd783afcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb681629c1125f7c713b",
+    "url": "/static/css/main.e98facc2.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb681629c1125f7c713b",
+    "url": "/static/js/main.0cd0e703.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
